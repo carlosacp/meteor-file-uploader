37f480498145873c0c2c1ce66eef70501c9494af
Carlos Pereira <carlos.acp@gmail.com>
2015-06-17 19:24:33 -0300

    export reactive-var

--- package.js
+++ package.js
@@ -13,8 +13,9 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
   api.use(['meteorhacks:picker', 'pfafman:multer'],'server', {weak: false, unordered: false});
+  api.use(['reactive-var'],'client', {weak: false, unordered: false});
   api.addFiles('file-uploader.js');
-  api.export(['Picker', 'multer']);
+  api.export(['Picker', 'multer', 'ReactiveVar']);
 });
 
 Package.onTest(function(api) {
